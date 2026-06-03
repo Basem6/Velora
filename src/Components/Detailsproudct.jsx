0fd6c95e1dabf623cbaa5b51f796075e6aeff401
Basem Mahmoud <basem556473@gmail.com>
2026-06-03 10:55:07 +0300

    ee

--- src/Components/Detailsproudct.jsx
+++ src/Components/Detailsproudct.jsx
@@ -98,12 +98,12 @@ export default function ProductDetails() {
                 Experience pure acoustic mastery. The LXP-900 combines
             </p>
             {/* Colors */}
-            <div className="pt-8" style={{ borderTop: '1px solid rgba(200,168,130,0.2)' }}>
+            <div className="pt-8 hidden" style={{ borderTop: '1px solid rgba(200,168,130,0.2)' }}>
                 <p className="text-xs uppercase tracking-[0.3em] mb-5" style={{ color: 'rgba(200,168,130,0.6)' }}>
                 Select Color : Obsidian
                 </p>
                 <div className="flex gap-4">
-                <button className="w-11 h-11 rounded-full bg-black ring-2 ring-black ring-offset-4" style={{ ringOffsetColor: '#0D0D0D' }} />
+                <button  className="w-11 h-11  rounded-full bg-black ring-2 ring-black ring-offset-4" style={{ ringOffsetColor: '#0D0D0D' }} />
 
                 <button className="w-11 h-11 rounded-full border hover:ring-2 hover:ring-offset-4 transition-all" style={{ backgroundColor: 'rgba(200,168,130,0.2)', borderColor: 'rgba(200,168,130,0.3)' }} />
 
